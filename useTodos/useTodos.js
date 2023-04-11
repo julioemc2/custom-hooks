6c357f70b691f1a6a1e520dff5f78dfc43feb99f
julioemc2 <julioemc2@gmail.com>
2023-04-10 20:56:52 -0400

    useTodos agregado

--- useTodos/useTodos.js
+++ useTodos/useTodos.js
@@ -1,19 +1,6 @@
 import { useEffect, useReducer } from "react";
-import { todoReducer } from "../08-useReducer/todoReducer";
+import { todoReducer } from "./todoReducer";
 
-
-// const initialState = [
-//     {
-//         id: new Date().getTime(),
-//         description: 'Recolectar la piedra del alma',
-//         done: false,
-//     },
-//     {
-//         id: new Date().getTime() * 3,
-//         description: 'Recolectar la piedra del tiempo',
-//         done: false,
-//     },
-// ];
 
 const init = () => {
     return JSON.parse( localStorage.getItem('todos')) || [];
